0a037575190c8f63441df8d9ddbfcb0384752dff
nHung-k419 <leronalluom@gmail.com>
2025-08-10 23:11:15 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./layouts/footer";
 import { PublicRoutes } from "./routes";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Chatbot from "./components/ChatBot/Chatbot";
