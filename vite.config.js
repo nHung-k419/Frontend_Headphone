c8eb42a24ca2e9b063244dacb87ce8c29008fd59
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-08-12 14:32:58 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist'
-  },
+ 
   server: {
     proxy: {
       "/api-province": {
