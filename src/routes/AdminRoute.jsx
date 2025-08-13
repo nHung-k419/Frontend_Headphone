4df050ac68302425795dd9883254ef95a37f1be3
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-08-13 23:18:12 +0700

    Update AdminRoute.jsx

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -2,13 +2,18 @@ import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 export default function AdminRoute({ children }) {
- const user = localStorage.getItem("User");
+  const user = localStorage.getItem("User");
+  
+  // const { id: idUser,Email } = user ? JSON?.parse(user) : "";
+  // const user = Cookies.get("User");
+
   if (!user) {
     return <Navigate to="/Auth/Login" replace />;
   }
 
  try {
     const userLocal = JSON.parse(user);
+    
     if (userLocal.Role !== "admin") {
       return <Navigate to="/Page404" replace />;
     }
